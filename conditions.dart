3d4a91abe2942b7229c1c34abb271c6e004e415b
ADOU FABRICE <fabriceadou5@gmail.com>
2022-06-24 23:19:29 +0000

    Les conditions

---
+++ conditions.dart
@@ -0,0 +1,47 @@
+void main() {
+  // Condition
+  //var nbStep = 3;
+
+  // if (nbStep < 3) {
+  //   print("Très faible");
+  // } else if (nbStep >= 3 && nbStep < 6) {
+  //   print("Moyenne");
+  // } else if (nbStep >= 6) {
+  //   print("Difficiel");
+  // }
+
+  // SWITCH
+  // switch (nbStep) {
+  //   case 1:
+  //     {
+  //       print("Lundi");
+  //       break;
+  //     }
+  //   case 2:
+  //     {
+  //       print("Mardi");
+  //       break;
+  //     }
+  //   case 3:
+  //     {
+  //       print("Mecredi");
+  //       break;
+  //     }
+  //   default:
+  //     {
+  //       print("Aucun");
+  //       break;
+  //     }
+  // }
+
+  // EXERCICE
+  var price = 6;
+
+  if (price < 5) {
+    print("C'est bon marché");
+  } else if (price >= 5 && price <= 10) {
+    print("C'est moyen");
+  } else if (price > 10) {
+    print("C'est cher");
+  }
+}
