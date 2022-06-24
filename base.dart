f34570f2d002e1dd3ffc6592cbbc3dbb2b2e67b1
ADOU FABRICE <fabriceadou5@gmail.com>
2022-06-24 21:22:38 +0000

    cours dart

---
+++ base.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Commentaire en dart
+  // print('Hello world'); // Afficher un méssage
+
+  // LES VARIABLES
+  // String name = "fabrice"; // Entier
+  // int nbPerson = 6 + 10; // Entier
+  // double chocoGramme = 13.4; // Réel
+  // bool isFavorite = true; // Booléan
+
+  // // OU
+
+  // var nom = "ADOU";
+
+  // print(nom);
+  // print(name); // Afficher une variable
+  // print(nbPerson);
+  // print("Nombre de personne $nbPerson"); //Concaténation
+  // print(chocoGramme);
+  // print(isFavorite);
+
+// -- EXERCICE
+  var texte = "J'aime les gâteaux";
+  print(texte);
+
+  texte = "Je n'aime pas les gâteaux";
+
+  print(texte);
+}
